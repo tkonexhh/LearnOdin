e715ed2bbc4ea0d880bfaa0797e652e4031e2b52
xhh <410830616@qq.com>
2021-09-15 21:08:31 +0800

    Button

--- Assets/Scripts/Usecase/Buttons/Button/ButtonDemo.cs
+++ Assets/Scripts/Usecase/Buttons/Button/ButtonDemo.cs
@@ -5,6 +5,14 @@ using Sirenix.OdinInspector;
 
 public class ButtonDemo : MonoBehaviour
 {
+    [Button("FoldoutButton", ButtonStyle.FoldoutButton)]
+    private void Default1(float a, string s, int[] c) { }
+    [Button("CompactBox", ButtonStyle.CompactBox)]
+    private void Default2(float a, string s, int[] c) { }
+    [Button("Box", ButtonStyle.Box)]
+    private void Default3(float a, string s, int[] c) { }
 
+    [Button]
+    private void Default(float a, string s, int[] c) { }
 
 }
